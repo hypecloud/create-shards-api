efb687a180d09bd9e283a95afb2c21499cc6a1e0
Tomek Marciniak <tmarciniakm@gmail.com>
2020-01-08 00:15:13 +0100

    feat(npmrc): add .npmrc

--- shards.config.js
+++ shards.config.js
@@ -1,20 +1,8 @@
 export default {
   models: {
-    realm: {
-      name: {
-        type: 'String',
-        defaultValue: 'IDK',
-        validation: value => value.length > 3
-      },
-      slug: {
-        type: 'String!',
-        validation: value => value.length > 3
-      },
-      posts: '[Post]'
-    },
     post: {
-      content: 'String!',
-      realm: 'Realm!'
+      title: 'String!',
+      content: 'String!'
     }
   }
 }
